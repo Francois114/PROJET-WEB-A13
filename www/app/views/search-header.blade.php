1049311e272fd35c196dc7eb07c9d9f5462da27d
Gabriel Santerre <gabrielsanterre@gmail.com>
2013-09-23 17:58:53 -0400

    ajout de l'alpha a la documentation!

--- www/app/views/search-header.blade.php
+++ www/app/views/search-header.blade.php
@@ -1,8 +1,14 @@
-<div class="right">
-	<div class="search-field-wrapper clearfix">
+<div class="search right">
+	<!-- <div class="search-field-wrapper clearfix">
 		{{Form::open(array('url' => 'search'));}}
 		{{Form::text('','Trouvez étudiant, projet et plus',array('class' => 'search-field'));}}
 		<input type="image" class="search-btn" src="img/search-nav-btn.png" alt="Submit" />
 		{{Form::close();}}
+	</div> -->
+	<div class="background">
+		{{Form::open(array('url' => 'search','id'=>'searchform'));}}
+		{{Form::text('','',array('class' => 'field'));}}
+		{{Form::close();}}
 	</div>
+	<div class="btn"></div>
 </div>
