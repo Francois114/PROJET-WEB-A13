512df58adb1abea24e2c015ee4545800182fd91d
Francois114 <fboivin114@gmail.com>
2013-09-26 13:53:04 -0400

    Ajout de ma partie

---
+++ alpha/site/js/form.js
@@ -0,0 +1,28 @@
+alert("lol");
+
+
+$(function() {
+            
+
+            $("#contenu div").hide();
+            $("#contenu h2").click(function(){
+                
+                $(this).next("div").slideToggle("slow").siblings("div:visible").slideUp("slow");
+                //$(this).toggleClass("active");
+                //$(this).toggleClass("notActive");
+                //$(this).siblings("h1").removeClass("active");
+                //$("#accordeon h1:first").addClass("notActive");
+                //$(this).switchClass("notActive","active",1000);
+                //$(this).siblings("h1").removeClass("active");
+                //$(this).siblings("h1").addClass("notActive");
+
+
+
+
+
+          
+                
+            });
+            
+    });
+
