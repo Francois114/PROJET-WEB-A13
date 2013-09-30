647e9233c7bfc87b9c8d814419172c227e0292ad
Gabriel Santerre <gabrielsanterre@gmail.com>
2013-09-30 08:12:31 -0400

    Parti francois

--- alpha/site/js/form.js
+++ alpha/site/js/form.js
@@ -1,4 +1,4 @@
-alert("lol");
+
 
 
 $(function() {
@@ -8,20 +8,7 @@ $(function() {
             $("#contenu h2").click(function(){
                 
                 $(this).next("div").slideToggle("slow").siblings("div:visible").slideUp("slow");
-                //$(this).toggleClass("active");
-                //$(this).toggleClass("notActive");
-                //$(this).siblings("h1").removeClass("active");
-                //$("#accordeon h1:first").addClass("notActive");
-                //$(this).switchClass("notActive","active",1000);
-                //$(this).siblings("h1").removeClass("active");
-                //$(this).siblings("h1").addClass("notActive");
 
-
-
-
-
-          
-                
             });
             
     });
